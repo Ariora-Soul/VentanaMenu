b5d2d11323f1cb4d0224d9dcefd89eac3adcca00
Ariora-Soul <ariorasoul@gmail.com>
2025-04-09 15:43:17 +0200

    I've change my objetive and remove code. Now im woking on basic aritmetic operations. Them i will improve that class with my actual skill.
    Signed-off-by: Ariora-Soul <ariorasoul@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,21 @@
-import javax.swing.*;
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        SwingUtilities.invokeLater(() -> {
+    //Crea objeto de la clase BasicArtimeticOperations
+    BasicArtimeticOperations operation = new BasicArtimeticOperations(4,2);
 
-            // creamos el objeto ventana con la clase JFRAME y su título
-
-            JFrame ventana = new JFrame("Ventana");
-
-            //Revisar este metodo
-            ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            //Hay que usar el metedo general de JavaX crear un nuevo objeto importando la clase ImageIcon
-            // y entre "" añadir la ruta del archivo.
-            ventana.setIconImage(new ImageIcon("C:/Users/Pablo/Desktop/Imagenes/server-icon.jpg").getImage());
-
-            //Las dimensiones de la ventana inicial
-            ventana.setSize(600, 600);
-
-            //Un objeto de la clase JLabel con un mensaje de texto centrado en pantalla
-            JLabel mensaje = new JLabel("Sape!", JLabel.CENTER);
-
-            //utiliza el objeto JFrame y utilizando el metodo add de la clase, invocamos alobjeto mensaje con el texto a mostrar.
-            ventana.add(mensaje);
-
-            // muestra o esconde el objeto de ventana según true o false.
-            ventana.setVisible(true);
-        });
+    System.out.println("Los numeros son: "+operation);
+    //Operaciones basicas
+    System.out.println("Suma de " + operation.getFirstNumber() + " + " +operation.getSecondNumber()
+            + " = " +operation.sum());
+    System.out.println("Resta de " + operation.getFirstNumber() + " - " +operation.getSecondNumber()
+                + " = " +operation.sustrac());
+    System.out.println("Multipliación de " + operation.getFirstNumber() + " * " +operation.getSecondNumber()
+                + " = " +operation.multiply());
+    System.out.println("División de " + operation.getFirstNumber() + " / " +operation.getSecondNumber()
+                + " = " +operation.division());
+    System.out.println("Modulo de " + operation.getFirstNumber() + " % " +operation.getSecondNumber()
+                + " = " +operation.modulo());
     }
 }
