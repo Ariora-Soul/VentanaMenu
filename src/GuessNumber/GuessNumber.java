0f8c0a37370b58d1226ffed9825300d15b33a9f1
Ariora-Soul <ariorasoul@gmail.com>
2025-04-25 16:40:40 +0200

    Completed GuessNumber for now!
    Signed-off-by: Ariora-Soul <ariorasoul@gmail.com>

--- src/GuessNumber/GuessNumber.java
+++ src/GuessNumber/GuessNumber.java
@@ -1,7 +1,15 @@
 package GuessNumber;
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumber {
+
+    private int adivina;
+
+    GuessNumber () {
+        this.adivina = randomGuessNumber();
+    }
+
     int randomGuessNumber() {
         Random guess = new Random();
         int random = guess.nextInt(10);
@@ -14,9 +22,18 @@ public class GuessNumber {
 
     public void tryGuessing(){
 
-//        if(random ==  ) {
-//
-//        }
+        for(int trys = 1; trys <= 3; trys++) {
+            Scanner insertedNumber = new Scanner(System.in);
+            System.out.print("Nº de intentos: " + trys);
+            System.out.print("\n¿Qué número es?: ");
+
+            if(adivina == insertedNumber.nextInt()) {
+                System.out.println("Correcto");
+                trys = 4;
+            } else {
+                System.out.println("Incorrecto...\n");
+            }
+        }
 
     }
 }
--- src/GuessNumber/GuessNumberMenu.java
+++ src/GuessNumber/GuessNumberMenu.java
@@ -6,37 +6,21 @@ import java.util.Scanner;
 public class GuessNumberMenu {
     public static void iniciar(Scanner scanner){
 
+        //Crea la clase y la variable que almacena el numero aleatorio.
         GuessNumber number = new GuessNumber();
         int adivina = number.randomGuessNumber();
+
+        //lo imprime
         System.out.println(adivina);
 
-        //CLI text
-        System.out.println("Se ha generado un número aleatorio entre el 0 y 10\n");
-        System.out.println("\nMENU Adivinar" + "\n###############");
-        System.out.println("1: ¡Quiero adivinarlo! \n2: Necesito pistas...\n");
-
        boolean menuStay = true;
-
         while (menuStay) {
 
-            System.out.print("Bla bla: ");
-
-            Scanner opcion = new Scanner(System.in);
-            switch (opcion.nextInt()){
-                case 1:
-                    number.tryGuessing();
-                    break;
-                case 2:
-                    number.hintPlease();
-                    break;
-
-                default:
-                    System.out.println("Esa opcion no es correcta");
-            }
+            number.tryGuessing();
 
             System.out.print("¿Desea otra operación? true o false: ");
-            Scanner newOperation = new Scanner(System.in);
-            if(newOperation.nextBoolean() == false) {
+            Scanner again = new Scanner(System.in);
+            if(again.nextBoolean() != true) {
                 menuStay = false;
             }
         }
